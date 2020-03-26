26abdfd1a1bbdf7333e075c9649a2e7496abdb0c
Adam Blinkinsop <blinks@acm.org>
2020-03-26 14:58:08 -0700

    Create the Theme oracle.
    Fix a bug with multiple visible oracles.

--- assets/oracle.js
+++ assets/oracle.js
@@ -1,20 +1,36 @@
-/* Oracle-ize the lists. */
+/** An oracle, pulled from the HTML. */
+function Oracle(list, button) {
+    this.list = list;
+    this.button = button;
+}
 
+/** Register our event handler, dealing with the vagaries of "this". */
+Oracle.prototype.register = function() {
+    var o = this;
+    o.button.addEventListener("click", function() { o.click(); });
+};
+
+/** A roll button was clicked, consult the oracle. */
+Oracle.prototype.click = function() {
+    var length = this.list.length;
+
+    // Remove all active entries.
+    for (var j = 0; j < length; j++) {
+        this.list[j].classList.remove("active", "near", "far");
+    }
+
+    var die = Math.floor(length * Math.random());
+    this.list[die].classList.add("active");
+    this.list[(die - 1 + length) % length].classList.add("near");
+    this.list[(die + 1) % length].classList.add("near");
+    this.list[length - die - 1].classList.add("far");
+};
+
+// Register all oracle data.
 var os = document.getElementsByClassName("oracle");
 for (var i = 0; i < os.length; i++) {
-    // Implement the button.
-    var t = os[i].getElementsByTagName("li");
-    var button = os[i].getElementsByClassName("roll")[0];
-    button.addEventListener("click", () => {
-        // Remove all active entries.
-        for (var j = 0; j < t.length; j++) {
-            t[j].classList.remove("active", "near", "far");
-        }
-
-        var die = Math.floor(t.length * Math.random());
-        t[die].classList.add("active");
-        t[(die - 1 + t.length) % t.length].classList.add("near");
-        t[(die + 1) % t.length].classList.add("near");
-        t[t.length - die - 1].classList.add("far");
-    });
+    var o = new Oracle(
+        os[i].getElementsByTagName("li"),
+        os[i].getElementsByClassName("roll")[0]);
+    o.register();
 }
