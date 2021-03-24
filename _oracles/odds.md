01f04489420817f1ce754a01d993a5e46c5541b5
Adam Blinkinsop <blinks@acm.org>
2021-03-24 14:40:48 -0700

    Add an "odds" (d%) table.

---
+++ _oracles/odds.md
@@ -0,0 +1,109 @@
+---
+title: Odds (d100)
+index: 20
+---
+
+Ask a yes/no question: Decide the odds of a 'yes', and roll to check the answer -- OR, pick two: envision two options. Rate one as 'likely', and roll to see if it is true. If not, it is the other.
+
+If something is _almost certain,_ 11+ is a yes. If _likely,_ 26+ is a yes. If _50/50,_ 51+ is a yes. If _unlikely,_ 76+ is a yes. If only a _small chance,_ 91+ is a yes.
+
+- 01
+- 02
+- 03
+- 04
+- 05
+- 06
+- 07
+- 08
+- 09
+- 10
+- **11** EXTREME/TWIST
+- 12
+- 13
+- 14
+- 15
+- 16
+- 17
+- 18
+- 19
+- 20
+- 21
+- **22** EXTREME/TWIST
+- 23
+- 24
+- 25
+- 26
+- 27
+- 28
+- 29
+- 30
+- 31
+- 32
+- **33** EXTREME/TWIST
+- 34
+- 35
+- 36
+- 37
+- 38
+- 39
+- 40
+- 41
+- 42
+- 43
+- **44** EXTREME/TWIST
+- 45
+- 46
+- 47
+- 48
+- 49
+- 50
+- 51
+- 52
+- 53
+- 54
+- **55** EXTREME/TWIST
+- 56
+- 57
+- 58
+- 59
+- 60
+- 61
+- 62
+- 63
+- 64
+- 65
+- **66** EXTREME/TWIST
+- 67
+- 68
+- 69
+- 70
+- 71
+- 72
+- 73
+- 74
+- 75
+- 76
+- **77** EXTREME/TWIST
+- 78
+- 79
+- 80
+- 81
+- 82
+- 83
+- 84
+- 85
+- 86
+- 87
+- **88** EXTREME/TWIST
+- 89
+- 90
+- 91
+- 92
+- 93
+- 94
+- 95
+- 96
+- 97
+- 98
+- **99** EXTREME/TWIST
+- **00** EXTREME/TWIST
--- assets/oracle.js
+++ assets/oracle.js
@@ -1,96 +1,95 @@
 function die(n) {
-    return Math.floor(Math.random() * n) + 1;
+  return Math.floor(Math.random() * n) + 1;
 }
 
 /** An oracle, pulled from the HTML. */
 function Oracle(list, button) {
-    this.list = list;
-    this.button = button;
+  this.list = list;
+  this.button = button;
 
-    var mx = /\s+x(\d+)/; // weight multiplier: x4 (for instance)
-    this.weight = 0;
-    for (var i = 0; i < this.list.length; i++) {
-        var li = this.list[i];
-        var ms = li.textContent.match(mx);
-        if (ms) {
-            li.textContent = li.textContent.replace(mx, "");
-            li.weight = parseInt(ms[1]);
-        } else {
-            li.weight = 1;
-        }
-        this.weight += li.weight;
+  var mx = /\s+x(\d+)/; // weight multiplier: x4 (for instance)
+  this.weight = 0;
+  for (var i = 0; i < this.list.length; i++) {
+    var li = this.list[i];
+    var ms = li.textContent.match(mx);
+    if (ms) {
+      li.textContent = li.textContent.replace(mx, "");
+      li.weight = parseInt(ms[1]);
+    } else {
+      li.weight = 1;
     }
+    this.weight += li.weight;
+  }
 }
 
-/** Register our event handler, dealing with the vagaries of "this". */
+/** Register our event handler. */
 Oracle.prototype.register = function() {
-    var o = this;
-    o.button.addEventListener("click", function() { o.click(); });
+  this.button.addEventListener("click", () => this.click());
 };
 
 /** Find the index that covers not more than this much weight. */
 Oracle.prototype.indexByWeight = function(weight) {
-    var length = this.list.length;
-    for (var i = 0; i < length; i++) {
-        if (weight < this.list[i].weight) {
-            return i;
-        }
-        weight -= this.list[i].weight;
+  var length = this.list.length;
+  for (var i = 0; i < length; i++) {
+    if (weight < this.list[i].weight) {
+      return i;
     }
+    weight -= this.list[i].weight;
+  }
 };
 
 /** A roll button was clicked, consult the oracle. */
 Oracle.prototype.click = function() {
-    var length = this.list.length;
-    var weight = this.weight;
+  var length = this.list.length;
+  var weight = this.weight;
 
-    // Remove all active entries.
-    for (var i = 0; i < length; i++) {
-        this.list[i].classList.remove("active", "near", "far");
-    }
+  // Remove all active entries.
+  for (var i = 0; i < length; i++) {
+    this.list[i].classList.remove("active", "near", "far");
+  }
 
-    // Roll a "die" based on the total weight of items.
-    var answer = this.indexByWeight(this.weight * Math.random());
-    this.list[answer].classList.add("active");
-    if (this.list.length > 3) {
-        this.list[(answer - 1 + length) % length].classList.add("near");
-        this.list[(answer + 1) % length].classList.add("near");
-    }
-    // Ironsworn allows reversing the die for a fourth item.
-    // But this doesn't work with super-flexible weighting.
-    //this.list[length - die - 1].classList.add("far");
+  // Roll a "die" based on the total weight of items.
+  var answer = this.indexByWeight(this.weight * Math.random());
+  this.list[answer].classList.add("active");
+  if (this.list.length > 3) {
+    this.list[(answer - 1 + length) % length].classList.add("near");
+    this.list[(answer + 1) % length].classList.add("near");
+  }
+  // Ironsworn allows reversing the die for a fourth item.
+  // But this doesn't work with super-flexible weighting.
+  //this.list[length - die - 1].classList.add("far");
 };
 
 // Register all oracle data.
 var os = $(".oracle");
 for (var i = 0; i < os.length; i++) {
-    var items = os[i].getElementsByTagName("li");
-    var o = new Oracle(
-        items,
-        os[i].getElementsByClassName("roll")[0]);
-    o.register();
+  var items = os[i].getElementsByTagName("li");
+  var o = new Oracle(
+    items,
+    os[i].getElementsByClassName("roll")[0]);
+  o.register();
 }
 
 // Implement the navbar's "Roll" button.
 function roll() {
-    $("#roll-result").fadeOut('fast', function() {
-        var e = $("#roll-result");
-        var action = die(6);
-        var challenge = [die(10), die(10)];
-        challenge.sort((a, b) => a - b);
+  $("#roll-result").fadeOut('fast', function() {
+    var e = $("#roll-result");
+    var action = die(6);
+    var challenge = [die(10), die(10)];
+    challenge.sort((a, b) => a - b);
 
-        e.removeClass("font-weight-bold text-danger text-warning text-success");
-        if (challenge[0] == challenge[1]) {
-            e.addClass('font-weight-bold');
-        }
-        if (action < challenge[0]) {
-            e.addClass('text-danger');
-        } else if (action < challenge[1]) {
-            e.addClass('text-warning');
-        } else {
-            e.addClass('text-success');
-        }
+    e.removeClass("font-weight-bold text-danger text-warning text-success");
+    if (challenge[0] == challenge[1]) {
+      e.addClass('font-weight-bold');
+    }
+    if (action < challenge[0]) {
+      e.addClass('text-danger');
+    } else if (action < challenge[1]) {
+      e.addClass('text-warning');
+    } else {
+      e.addClass('text-success');
+    }
 
-        e.text(`[${action}] vs <${challenge[0]}> <${challenge[1]}>`).fadeIn();
-    });
+    e.text(`[${action}] vs <${challenge[0]}> <${challenge[1]}>`).fadeIn();
+  });
 }
